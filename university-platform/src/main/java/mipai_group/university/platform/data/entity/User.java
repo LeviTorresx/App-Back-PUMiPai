e8ba6488dd4caee9ec187920c55cdd44f872a7b5
IAmLev1 <levisj.aguiar@udea.edu.co>
2024-10-14 23:00:39 -0500

    Creation of user entity and repository [003]

--- university-platform/src/main/java/mipai_group/university/platform/data/entity/User.java
+++ university-platform/src/main/java/mipai_group/university/platform/data/entity/User.java
@@ -1,4 +1,36 @@
 package mipai_group.university.platform.data.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String dni;
+    private String name;
+    private String lastName;
+    private String email;
+    private String userName;
+    private String password;
+    private String phoneNumber;
+    private String city;
+    private int semester;
+
+    //private Careers career;
+    //private <List> courses;
+    //private History academicHistory;
+    //private float mean;
+    //private int credits;
+    //possible changes...
 }
--- university-platform/src/main/java/mipai_group/university/platform/data/repository/UserRepository.java
+++ university-platform/src/main/java/mipai_group/university/platform/data/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package mipai_group.university.platform.data.repository;
 
-public interface UserRepository {
+import mipai_group.university.platform.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
-git 
